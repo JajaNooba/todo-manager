29fc7b7aa3e4a4dc848a85e26053cfa979f395ef
Bartosz Kuś <kusbartek30@gmail.com>
2023-10-16 20:49:07 +0200

    Added basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+use todo_manager::{ Cli, Commands };
+
 fn main() {
+
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Add(args) => println!("Add {}", args.task_name),
+        Commands::Remove(args) => println!("Remove {}", args.task_name),
+        Commands::Complete(args) => println!("Complete {}", args.task_name),
+        Commands::Important(args) => println!("Important {}", args.task_name),
+        Commands::Show => println!("Show")
+    }
+
     println!("Hello, world!");
 }
